815eb1098a63c9401ea3f62e414576b1f5bdd730
BhagyaLiyanage <136679416+BhagyaLiyanage@users.noreply.github.com>
2023-08-29 00:04:03 +0530

    Add files via upload

---
+++ Q2.scala
@@ -0,0 +1,17 @@
+package LabSheet_10
+
+object Q2 {
+  def main(args: Array[String]): Unit = {
+    val inputWords = List("apple", "banana", "cherry", "date")
+    val totalLetterOccurrences = countLetterOccurrences(inputWords)
+    println(s"Total count of letter occurrences: $totalLetterOccurrences")
+  }
+
+  def countLetterOccurrences(words: List[String]): Int = {
+    val wordLengths = words.map(_.length)
+    val totalOccurrences = wordLengths.reduce((len1, len2) => len1 + len2)
+    totalOccurrences
+  }
+}
+
+
